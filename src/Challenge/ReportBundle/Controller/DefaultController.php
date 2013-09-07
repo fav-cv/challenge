d854edd98f038b82e29f16abce786267b7f017a5
Flavio <Flavio@COCO>
2013-09-07 17:04:16 +0200

    Entities working

--- src/Challenge/ReportBundle/Controller/DefaultController.php
+++ src/Challenge/ReportBundle/Controller/DefaultController.php
@@ -6,14 +6,40 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
-{
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller {
+
     /**
      * @Route("/hello/{name}")
      * @Template()
      */
-    public function indexAction($name)
-    {
+    public function helloAction($name) {
         return array('name' => $name);
     }
+
+    /**
+     * @Route("/countries")
+     * @Template()
+     */
+    public function countriesAction() {
+
+        $countries = $this->getDoctrine()->getManager()
+                ->getRepository('ChallengeReportBundle:Country')
+                ->findAll();
+        
+        $content = '';        
+        foreach ($countries as $country) {
+            $content .= $country->getCountryId() . 
+                    ' | ' . $country->getCode() . 
+                    ' | ' . $country->getCountry() . "\n";
+        }
+        
+        $response = new Response();
+        $response->setContent($content);
+        $response->headers->set('Content-Type', 'text/plain');
+        
+        return $response;
+    }
+
 }
